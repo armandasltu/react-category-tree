fa736b63e6c4e3ae5805bcd2cc1d3c5a278b8d46
Armandas Motuzas <armandas.m@mworker.no>
2020-07-10 13:59:37 +0300

    Add Recursive Tree list. Fix eslint errors.

--- src/components/RecursiveTree.tsx
+++ src/components/RecursiveTree.tsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React from "react";
+import categoryList from "mocks/categoryList";
+import { Category } from "types";
+import Typography from "@material-ui/core/Typography";
+import { Box } from "@material-ui/core";
 
 const RecursiveTree: React.FC = () => {
-    return <>This is a list<>;
-}
+  const CategoryItem = ({ name, child }: Category) => (
+    <Box ml={2}>
+      {`- ${name}`}
+      {child?.map((category: Category, index) => (
+        <CategoryItem {...category} key={index} />
+      ))}
+    </Box>
+  );
+
+  return (
+    <div>
+      <Typography variant="h6" component="h2">
+        Recursive list of categories:
+      </Typography>
+      <div>
+        {categoryList.map((category: Category, index) => (
+          <CategoryItem {...category} key={index} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default RecursiveTree;
---
+++ src/mocks/categoryList.ts
@@ -0,0 +1,28 @@
+const categoryList = [
+  {
+    name: "Lorem",
+    child: [
+      {
+        name: "Ipsum",
+      },
+      {
+        name: "Dolor",
+        child: [
+          {
+            name: "Orci",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Sit",
+    child: [
+      {
+        name: "Amet",
+      },
+    ],
+  },
+];
+
+export default categoryList;
